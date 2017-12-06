46bebcfb8fb2528e2cc30b3252a210c4d65cdb27
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-12-05 20:43:52 -0500

    clean up

--- ts/problem6.ts
+++ ts/problem6.ts
@@ -4,62 +4,62 @@ class Problem6 {
   displayEle: any;
   inputEle: any;
 
-  constructor(){
+  constructor() {
     this.displayEle = document.getElementById("display");
     this.inputEle = document.getElementById("user_input");
   }
-  init(){
+  init() {
     this.askForTime();
   }
 
-  convertInputToTime(){
+  convertInputToTime() {
     let timeIN: Time = this.createTime(this.inputEle.value);
     timeIN.convertToMilitary();
-    this.displayEle.innerHTML += "<br>"+ this.convertTimeToString(timeIN)+"<br>";
+    this.displayEle.innerHTML += "<br>" + this.convertTimeToString(timeIN) + "<br>";
     this.askForTime();
   }
 
-  convertTimeToString(time:Time):string{
-    return this.convertNumToString(time.getHour())+" Hundred and "+this.convertNumToString(time.getMinute())+" Hours";
+  convertTimeToString(time: Time): string {
+    return this.convertNumToString(time.getHour()) + " Hundred and " + this.convertNumToString(time.getMinute()) + " Hours";
   }
 
-  convertNumToString(number: number):string{
-    if(number < 10){
-      return "Zero "+ this.getSecondDigit(number);
-    }else if(number < 20){
+  convertNumToString(number: number): string {
+    if (number < 10) {
+      return "Zero " + this.getSecondDigit(number);
+    } else if (number < 20) {
       return this.getTeenNumber(number);
-    }else{
+    } else {
       return this.convertNumberOverTwenty(number);
     }
   }
 
-  convertNumberOverTwenty(number:number):string{
-    let convertNumber:number = Math.floor(number/10);
-    let secondDigit:number = number-(convertNumber*10);
-    return this.getfirstDigit(convertNumber)+" "+this.getSecondDigit(secondDigit);
+  convertNumberOverTwenty(number: number): string {
+    let convertNumber: number = Math.floor(number / 10);
+    let secondDigit: number = number - (convertNumber * 10);
+    return this.getfirstDigit(convertNumber) + " " + this.getSecondDigit(secondDigit);
   }
 
-  getfirstDigit(digit:number):string{
-    let options:string[] = ["Twenty","Thirty","Forty","Fifty"];
-    return options[digit-2];
+  getfirstDigit(digit: number): string {
+    let options: string[] = ["Twenty", "Thirty", "Forty", "Fifty"];
+    return options[digit - 2];
   }
 
-  getTeenNumber(number:number):string{
-    let options: string[] = ["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"];
-    return options[number-10];
+  getTeenNumber(number: number): string {
+    let options: string[] = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+    return options[number - 10];
   }
 
-  getSecondDigit(digit:number):string{
-    let options: string[] = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+  getSecondDigit(digit: number): string {
+    let options: string[] = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
     return options[digit];
   }
 
-  createTime(stringTime: string):Time{
-    let timeIsAM: boolean = (stringTime.substr(stringTime.length-2).toLowerCase() == 'am');
-    let timeSplit: string[] = stringTime.substr(0,stringTime.length-2).split(":");
-    return new Time(parseInt(timeSplit[0]),parseInt(timeSplit[1]),timeIsAM);
+  createTime(stringTime: string): Time {
+    let timeIsAM: boolean = (stringTime.substr(stringTime.length - 2).toLowerCase() == 'am');
+    let timeSplit: string[] = stringTime.substr(0, stringTime.length - 2).split(":");
+    return new Time(parseInt(timeSplit[0]), parseInt(timeSplit[1]), timeIsAM);
   }
-  askForTime(){
+  askForTime() {
     this.displayEle.innerHTML += "What time would you like to convert?";
   }
 }
--- ts/time.ts
+++ ts/time.ts
@@ -1,33 +1,33 @@
-class Time{
+class Time {
   hour: number;
   minute: number;
   isAM: boolean;
 
-  constructor(hour:number,minute:number,isAM:boolean){
+  constructor(hour: number, minute: number, isAM: boolean) {
     this.hour = hour;
     this.minute = minute;
     this.isAM = isAM;
   }
 
-  setHour(newHour:number){
+  setHour(newHour: number) {
     this.hour = newHour;
   }
-  getHour(): number{
+  getHour(): number {
     return this.hour;
   }
-  getMinute(): number{
+  getMinute(): number {
     return this.minute;
   }
-  getIsAM(): boolean{
+  getIsAM(): boolean {
     return this.isAM;
   }
 
-  convertToMilitary():void{
-    if(this.isAM && (this.hour == 12)){
+  convertToMilitary(): void {
+    if (this.isAM && (this.hour == 12)) {
       this.hour = 0;
-    }else if((!this.isAM) && this.hour != 12){
+    } else if ((!this.isAM) && this.hour != 12) {
       this.hour += 12;
-      if(this.hour == 24){
+      if (this.hour == 24) {
         this.hour = 0;
       }
     }
